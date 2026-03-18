f7511ff5b3a1d40968c02cec775b52896dd7779c
Angelica Grace Simbran <jingkaysimbran@gmail.com>
2026-03-18 21:33:57 +0800

    created a confirmation modal for the logout

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -7,6 +7,8 @@ import { auth } from "../firebase";
 export default function SideBar() {
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(true);
+  const [showLogoutConfirmation, setShowLogoutConfirmation] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleLogout = async () => {
     try {
@@ -84,13 +86,62 @@ export default function SideBar() {
         </Link>
 
         <button
-          onClick={handleLogout}
+          disabled={loading}
+          onClick={()=>{
+            setShowLogoutConfirmation(true);
+          }}
           className="flex items-center gap-2 p-2 rounded-lg hover:bg-blue-100 text-red-500 w-full text-left"
         >
           <LogOut size={18} />
           {isOpen && <span>Logout</span>}
         </button>
       </nav>
+
+          {/* DELETE CONFIRMATION MODAL */}
+          {showLogoutConfirmation && (
+
+            <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center">
+
+              <div className="bg-white p-6 rounded-xl shadow-xl w-120">
+
+                <h2 className="text-xl font-bold mb-2">
+                  Confirm Logout
+                  
+                </h2>
+                <hr></hr>
+
+                <p className="mb-2 mt-4">
+                  You are about to log out of your session. Any unsaved changes may be lost. 
+                
+                </p>
+
+                <p>Do you want to continue?</p>
+
+                <div className="flex justify-end gap-3">
+
+                  <button
+                    onClick={()=>setShowLogoutConfirmation(false)}
+                    className="px-4 py-2 bg-gray-300 rounded"
+                  >
+                    Cancel
+                  </button>
+
+                  <button
+                    onClick={handleLogout}
+                    disabled={loading}
+                    className="px-4 py-2 bg-red-500 text-white rounded"
+                  >
+                    {loading ? "Logging out..." : "Log out"}
+                  </button>
+
+                </div>
+
+              </div>
+
+            </div>
+
+          )}
     </aside>
+    
   );
 }
